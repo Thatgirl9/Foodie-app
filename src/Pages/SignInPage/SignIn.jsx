24e5391bda1951b050141b0c37a5c1a0f53aca31
Favour Ukonu <favouroukonu@gmail.com>
2024-01-03 20:54:12 +0100

    SignIn page added

---
+++ src/Pages/SignInPage/SignIn.jsx
@@ -0,0 +1,11 @@
+import "./signin.css";
+
+const SignIn = () => {
+  return (
+    <div className="bg-red-500">
+      <h1>Sign In</h1>
+    </div>
+  );
+};
+
+export default SignIn;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import WelcomePage from "./Pages/Welcomepage/WelcomePage";
+import SignIn from "./Pages/SignInPage/SignIn";
 
 const router = createBrowserRouter([
   {
