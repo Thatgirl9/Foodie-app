0f137c2f418c3f50014f28ef846b9344d19b5854
Favour Ukonu <favouroukonu@gmail.com>
2024-01-06 21:45:04 +0100

    SignUp functionalities implemented

--- src/Pages/SignUpPage/SignUp.jsx
+++ src/Pages/SignUpPage/SignUp.jsx
@@ -2,11 +2,19 @@ import "./signup.css";
 
 import ArrowLeft from "../../assets/SignIn/bi_arrow-left.png";
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import PasswordInput from "../../Components/PasswordInput";
 
 const SignUp = () => {
+  // Routing
   const navigate = useNavigate();
+  // Validating Email and Name.
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
 
+  // Go back to previous page
   const handleGoBack = () => {
     if (navigate(-1) === navigate("./welcomepage")) {
       navigate("./firstpage");
@@ -15,6 +23,44 @@ const SignUp = () => {
     }
   };
 
+  // Validating Name Function
+  const handleNameValid = (event) => {
+    const newName = event.target.value;
+
+    // Check if the entered name contains a number or a special character
+    if (/\d/.test(newName) || /[!@#$%^&*(),.?":{}|<>]/g.test(newName)) {
+      setNameError("Name cannot contain numbers");
+    } else {
+      setNameError("");
+    }
+
+    // Check if the entered name contains a special character
+    // if (/[!@#$%^&*(),.?":{}|<>]/g.test(newName)){
+    //   setNameError('Name cannot contain special characters');
+    // } else{
+    //   setNameError('');
+    // }
+
+    // Update the name state
+    setName(newName);
+  };
+
+  // Validating Email Function
+  const handleEmailValid = (event) => {
+    const newEmail = event.target.value;
+
+    //Check if the entered email does not contain the @ symbol
+    if (!/@/.test(newEmail)) {
+      setEmailError("Email must contain @ symbol");
+    } else {
+      setEmailError("");
+    }
+
+    // Update the email state
+    setEmail(newEmail);
+  };
+
+  // Preventing reloading of the page
   const formSubmit = (e) => {
     e.preventDefault();
   };
@@ -50,10 +96,20 @@ const SignUp = () => {
             <input
               type="text"
               name="name"
-              id="email"
-              className="w-full h-14 rounded-md px-4 bg-inputBtn font-body placeholder:font-medium placeholder:text-placeholder outline-none"
+              id="name"
+              value={name}
+              inputMode="text"
+              onChange={handleNameValid}
+              className={`w-full h-14 rounded-md px-4 bg-inputBtn font-body placeholder:font-medium placeholder:text-placeholder outline-none border ${
+                nameError ? "border-red-500" : "border-inputBtn"
+              }`}
               placeholder="Your full name"
             />
+            {nameError && (
+              <p className="text-red-500 text-sm font-header mt-1">
+                {nameError}
+              </p>
+            )}
           </div>
 
           <div>
@@ -67,9 +123,19 @@ const SignUp = () => {
               type="email"
               name="email"
               id="email"
-              className="w-full h-14 rounded-md px-4 bg-inputBtn font-body placeholder:font-medium placeholder:text-placeholder outline-none"
+              value={email}
+              inputMode="email"
+              onChange={handleEmailValid}
+              className={`w-full h-14 rounded-md px-4 bg-inputBtn font-body placeholder:font-medium border placeholder:text-placeholder outline-none ${
+                emailError ? "border-red-500" : "border-inputBtn"
+              }`}
               placeholder="Your email"
             />
+            {emailError && (
+              <p className="text-red-500 text-sm font-header mt-1">
+                {emailError}
+              </p>
+            )}
           </div>
 
           <div>
@@ -107,7 +173,7 @@ const SignUp = () => {
       <div className="flex justify-center mt-14">
         <p className="font-body text-center">
           <span className="font-medium text-sm text-placeholder">
-            By clicking Sign In, you agree to our
+            By clicking Sign Up, you agree to our
           </span>
           <br></br>
           <span className="font-bold text-secondPageBtn">
