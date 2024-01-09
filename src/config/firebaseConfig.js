ced43b3d8fe7cc9bbc230403ea8d44b356cfc056
Favour Ukonu <favouroukonu@gmail.com>
2024-01-09 22:05:14 +0100

    Feat: Firebase added for authentication

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app';
+// import firebase from 'firebase/app';
+import 'firebase/auth';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  //...
+};
+
+const app = initializeApp(firebaseConfig);
